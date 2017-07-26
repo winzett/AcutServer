64fbfce2c4d84189fa79fcadd7135d821b01164d
woongjae lee <winzett0@gmail.com>
2017-07-26 23:13:56 +0900

    메일 코드 변경

--- acutserver/core/mailer.py
+++ acutserver/core/mailer.py
@@ -3,14 +3,15 @@
 import smtplib
 from email.mime.text import MIMEText
 
-smtp = smtplib.SMTP('smtp.gmail.com', 587)
-smtp.ehlo()      # say Hello
-smtp.starttls()  # TLS 사용시 필요
-smtp.login('lee@live.com', 'password')
+def test_mail :
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()      # say Hello
+    smtp.starttls()  # TLS 사용시 필요
+    smtp.login('lee@live.com', 'password')
 
-msg = MIMEText('본문 테스트 메시지')
-msg['Subject'] = '테스트'
-msg['To'] = 'winzett0@gmail.com'
-smtp.sendmail('winzett0@gmail.com', 'winzett0@gmail.com', msg.as_string())
+    msg = MIMEText('본문 테스트 메시지')
+    msg['Subject'] = '테스트'
+    msg['To'] = 'winzett0@gmail.com'
+    smtp.sendmail('winzett0@gmail.com', 'winzett0@gmail.com', msg.as_string())
 
-smtp.quit()
+    smtp.quit()
